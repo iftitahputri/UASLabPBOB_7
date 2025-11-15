1ea7aa4ecb2d8120621f16b965b7859a0fe7a01c
gita-hfdzh <brigitaagnil@gmail.com>
2025-11-15 21:39:19 +0700

    tambah file di folder detailpesanan

---
+++ DetailPesanan/DetailPesanan.java
@@ -0,0 +1,37 @@
+package DetailPesanan;
+
+public class DetailPesanan {
+    private MenuItem item;      
+    private Jumlah jumlah;
+    private Catatan catatan;
+    private Subtotal subtotal;
+
+    public DetailPesanan(MenuItem item, int jumlah, String catatan) {
+        this.item = item;
+        this.jumlah = new Jumlah(jumlah);
+        this.catatan = new Catatan(catatan);
+        this.subtotal = new Subtotal(item, this.jumlah);
+    }
+
+    public MenuItem getItem() { 
+        return item; 
+    }
+    public Jumlah getJumlah() { 
+        return jumlah; 
+    }
+    public Catatan getCatatan() { 
+        return catatan; 
+    }
+    public Subtotal getSubtotal() { 
+        return subtotal; 
+    }
+
+    @Override
+    public String toString() {
+        if (catatan.isEmpty()) {
+            return item.getNama() + " x " + jumlah.getValue();
+        } else {
+            return item.getNama() + " x " + jumlah.getValue() + " (" + catatan.getKeterangan() + ")";
+        }
+    }
+}
